447e14390f918f5102f2ac2f009bbd0d20cfb137
samtasticc <samantha.henesy@gmail.com>
2025-02-19 22:07:34 -0500

    create DTL templates for base and about html pages

--- booknest/main_app/views.py
+++ booknest/main_app/views.py
@@ -5,4 +5,4 @@ def home(request):
     return HttpResponse('<h1>Welcome to the BookNest📖</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About to the BookNest📖</h1>')
+    return render(request, 'about.html')
