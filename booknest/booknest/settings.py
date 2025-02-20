e2a7620d93ea749fe7de439fac1293f40f019ebf
samtasticc <samantha.henesy@gmail.com>
2025-02-20 01:09:28 -0500

    create booknest db, migrate, update db in settings.py

--- booknest/booknest/settings.py
+++ booknest/booknest/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# load environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +81,16 @@ WSGI_APPLICATION = 'booknest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'booknest',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
+
 
 
 # Password validation
--- booknest/main_app/models.py
+++ booknest/main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Author:
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    bio = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return self.name
